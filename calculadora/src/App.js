995420be1dd46f595a35af26cbba4af191a99fe5
kimha1030 <1478@holbertonschool.com>
2022-04-25 17:56:58 -0500

    Proyecto Calculadora

---
+++ calculadora/src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+import logo from "./img/logo.png";
+import Boton from "./componentes/Boton.js";
+import Pantalla from "./componentes/Pantalla.js";
+import BotonClear from "./componentes/BotonClear.js";
+import { useState } from "react";
+import { evaluate } from "mathjs";
+
+function App() {
+  const [input, setInput] = useState("");
+
+  const agregarInput = (valor) => {
+    parseInt(setInput(input + valor));
+  };
+
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("No ha ingresado valores");
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="logo-contenedor">
+        <img src={logo} className="logo-img" alt="Logo de FreeCodeCamp" />
+      </div>
+      <div className="calculadora-contenedor">
+        <div className="calculadora-teclas">
+          <Pantalla input={input} />
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+          <BotonClear manejarClear={() => setInput("")}>Clear</BotonClear>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
